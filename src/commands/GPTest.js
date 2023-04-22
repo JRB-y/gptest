229a4b97848260826d9d8d5d4fad58b333dda665
Jrb <jrb.youssef@gmail.com>
2023-04-22 02:29:43 +0100

    refactor: reafactor the prompt

--- src/commands/GPTest.js
+++ src/commands/GPTest.js
@@ -1,6 +1,10 @@
 const vscode = require('vscode');
 const { initOpenAI } = require('../openai');
 
+let promptStart = `You are the most advanced [[languageId]] QA software AI, write an externa test file for: `;
+let promptLib = `Your response should be in [[library]] library`;
+const promptEnd = `Your response should include ONLY CODE and VALID [[languageId]] syntaxe!`;
+
 module.exports = vscode.commands.registerCommand('gptest.gptest', async function () {
   const editor = vscode.window.activeTextEditor;
   if (!editor) vscode.window.showErrorMessage('You need an active text editor to use GPTest');
@@ -14,20 +18,25 @@ module.exports = vscode.commands.registerCommand('gptest.gptest', async function
     }, async (progress) => {
       progress.report({ message: 'GPTest is writting the tests...' });
 
-
       const content = editor.document.getText().replace(/\n/g, ' ').replace(/\s+/g, ' ').replace(/\t+/g, '').trim();
       const languageId = editor.document.languageId;
-      const prompt = `Write a test file for the following ${languageId} file, use a popular ${languageId} testing library. ONLY CODE! The file: ${content}`;
+
+      let prompt = promptStart.replace('[[languageId]]', languageId) + content + '\n';
+
+      const library = vscode.workspace.getConfiguration().get('gptest.lib');
+      if (library) {
+        prompt += `${promptLib.replace('[[library]]', library)}`;
+      }
+
+      prompt += `${promptEnd.replace('[[languageId]]', languageId)}`;
+      
 
       const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
-          {
-            role: 'user',
-            content: prompt,
-          }
+          { 'role': 'user', 'content': prompt },
         ],
-        temperature: 0,
+        temperature: 0.7,
       });
 
       const doc = await vscode.workspace.openTextDocument({
